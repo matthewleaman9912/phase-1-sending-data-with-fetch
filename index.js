9a5050746e84c62e9785e0f4bf4e0572601e7183
MatthewLeaman <leaman.30@osu.edu>
2024-01-12 00:35:51 -0500

    complete challenges

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+function submitData (name, email) {
+    const data = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    }
+    return fetch('http://localhost:3000/users', data) 
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(object){
+            let info = document.querySelector("body")
+            info.innerHTML = object.id
+        })
+        .catch(function(error) {
+            let message = document.querySelector("body")
+            message.innerHTML = error;
+        })
+}
